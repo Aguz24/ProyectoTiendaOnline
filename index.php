124d260ac40ee6eff7e6433a19c8e3189748539f
Agus <sagu9168@gmail.com>
2025-04-11 17:10:25 -0300

    Escribi que tan inutil fue italia en la segunda guerra mundial

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
-hola
+La participacion de italia en la segunda guerra mundial fue casi que nula pues su armamento sumamente inferior los condenos a ser intransendentes
+esto condeno a sus aliados (en especial a alemania) se podria decir que tienes que elegir mejor a tus aliados que a tus enemigos
